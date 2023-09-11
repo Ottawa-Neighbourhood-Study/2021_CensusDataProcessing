b07ffe886566b670d3eaba47373305a7145ce300
Kady Carr <carrkady1@gmail.com>
2023-09-11 14:56:22 -0400

    forQs

--- r/Make_quintile_data.R
+++ r/Make_quintile_data.R
@@ -13,13 +13,51 @@ dictionary_indiv<- as.tibble(read_csv("outputs/pq outputs/processed/clean_pq_tgp
 colnames(dictionary_indiv) <- c("VAR_ID","type","category", "description")
 dictionary_indiv<- dictionary_indiv[-c(1,2,nrow(dictionary_indiv)),]
 
-#create clean data file (insert source file for each here below)
-raw_youth <- read_csv("outputs/pq outputs/processed/clean_pq_tgp_age_15to24-2023-09-08.csv")
-colnames(raw_youth) <- raw_youth[1,]
-colnames(raw_youth)[1] <- "ONS_Name"
-colnames(raw_youth)[2] <- "ONS_ID"
-raw_youth <- raw_youth[-c(1,2,3),]
+filetouse<- "outputs/pq outputs/processed/clean_pq_tgp_age_15to24-2023-09-08.csv"
+#create clean data file (insert source file for each here above)
+raw_file <- read_csv(filetouse)
+colnames(raw_file) <- raw_file[1,]
+colnames(raw_file)[1] <- "ONS_Name"
+colnames(raw_file)[2] <- "ONS_ID"
+raw_file <- raw_file[-c(1,2,3),]
 
-filter(raw_youth, grep())
 
-?grep
+#cleaning the file
+clean_file<- as.data.frame(raw_file[,-1] %>%
+  t())
+
+clean_file<-clean_file %>%
+  mutate(
+    VAR_ID = rownames(clean_file)
+  ) %>% relocate("VAR_ID",.before = V1)
+clean_file[1,1]="VAR_ID"
+colnames(clean_file) <- clean_file[1,]
+clean_file<- clean_file[-1,]
+clean_file<-subset(clean_file, select =-65)
+
+clean_file<-as.tibble(clean_file)
+colnames(clean_file)
+
+#subsetting dictionary to include only %
+Percentages1<- filter(dictionary_indiv, grepl("Percentage",dictionary_indiv$type,))
+Percentages2<- filter(dictionary_indiv, grepl("Median",dictionary_indiv$type,))
+Percentages3<- filter(dictionary_indiv, grepl("Average",dictionary_indiv$type,))
+compare_dictionary<- as.tibble(rbind(Percentages1,Percentages2,Percentages3))
+
+comparable_data<- left_join(
+  as.tibble(compare_dictionary),
+  as.tibble(clean_file),
+  by="VAR_ID"
+)
+
+comparable_data2 <- as.data.frame(t(comparable_data))
+colnames(comparable_data2)<- comparable_data2[1,]
+comparable_data2 <- comparable_data2[-c(1,2,3,4),]
+compare3 <-comparable_data2
+
+#convert to numeric
+for (i in 1:40) {
+  compare3[,i] <- as.numeric(compare3[,i])
+  print(typeof(compare3[,1]))
+}
+
