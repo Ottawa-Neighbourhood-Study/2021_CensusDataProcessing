3a83ef69b2e7bbcf1e51ffebebeef9fee918e1d2
Kady Carr <carrkady1@gmail.com>
2023-09-11 13:31:30 -0400

    Update Make_quintile_data.R

--- r/Make_quintile_data.R
+++ r/Make_quintile_data.R
@@ -4,15 +4,22 @@ library(tidyr)
 library(tidyverse)
 library(dplyr)
 
-dictionary_indiv<- read_csv("outputs/pq outputs/processed/clean_pq_tgp_age_15to24-2023-09-08.csv")[c(1,2,3),]
+#create dictionary using any of the data files
+dictionary_indiv<- as.tibble(read_csv("outputs/pq outputs/processed/clean_pq_tgp_age_15to24-2023-09-08.csv")[c(1,2,3),] %>%
+  t()) %>%
+  mutate(
+    description =as.tibble(colnames(read_csv("outputs/pq outputs/processed/clean_pq_tgp_age_15to24-2023-09-08.csv")))
+  )
+colnames(dictionary_indiv) <- c("VAR_ID","type","category", "description")
+dictionary_indiv<- dictionary_indiv[-c(1,2,nrow(dictionary_indiv)),]
 
-testdf <- dictionary_indiv %>%
-  pivot_longer()
-
+#create clean data file (insert source file for each here below)
 raw_youth <- read_csv("outputs/pq outputs/processed/clean_pq_tgp_age_15to24-2023-09-08.csv")
 colnames(raw_youth) <- raw_youth[1,]
 colnames(raw_youth)[1] <- "ONS_Name"
 colnames(raw_youth)[2] <- "ONS_ID"
-dictionary <- raw_youth[c(1,2,3),]
+raw_youth <- raw_youth[-c(1,2,3),]
 
-raw_youth <- raw_youth[-c(1,2,3)]
+filter(raw_youth, grep())
+
+?grep
