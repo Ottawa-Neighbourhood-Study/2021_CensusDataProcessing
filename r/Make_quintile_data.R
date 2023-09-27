dabd26a351e0eda1040d1b6aed7a3bc9fd492d7d
anzo-n <anzo.n7@gmail.com>
2023-09-27 12:18:52 -0400

    changes to make quintile data
    Changes to make quintile data R script for non-census data

--- r/Make_quintile_data.R
+++ r/Make_quintile_data.R
@@ -31,11 +31,24 @@ quintile_dictionary <- quintile_dictionary %>%
 ncol(quintile_dictionary)
 nrow(quintile_dictionary)
 
+#filter dictionary by quintilable
+
+quintile_dictionary2 <- filter(quintile_dictionary, Quintilable == "yes")
+
+clean_file2<- as.data.frame(raw_file %>%
+                             t())
+
+clean_file3 <- clean_file2 |>
+    mutate(data_ID=rownames(clean_file2)) |>
+     relocate("data_ID",.before = "V1")
+clean_file3[1,1] = "data_ID"
+colnames(clean_file3) = clean_file3[1,]
+
 ## Join here
 comparable_data<- left_join(
-  as.tibble(quintile_dictionary),
-  as.tibble(clean_file),
-  by="VAR_ID"
+  as.tibble(quintile_dictionary2),
+  as.tibble(clean_file3),
+  by="data_ID"
 )
 
 # comparable_data<- clean_file    ---a little different for the non-census data here
@@ -43,9 +56,7 @@ comparable_data2 <- as.data.frame(t(comparable_data))
 colnames(comparable_data2)<- comparable_data2[1,]
 
 
-
-comparable_data2 <- comparable_data2[-1,]
-compare3 <-comparable_data2[-c(1,2,3,4,5,6,7,8,9),]
+compare3 <-comparable_data2[-c(1:7),]
 
 compare3_backup<-compare3
 #convert to numeric
@@ -91,6 +102,9 @@ for (i in 1:x) {
   print(ranked[,i])
 }
 
+#count how many Q1s, Q2, Q3, etc. for each column. Look up how to do
+
+
 #transpose - not sure if this part below belongs or not (resolving conflicts)
 #quintiles<- as.data.frame(t(ranked))
 #quintiles<-quintiles %>%
@@ -100,13 +114,14 @@ for (i in 1:x) {
 #compare3 <- as.data.frame(compare3) %>%
 #  t()
 
-quintiles<-quintiles%>% 
-  relocate("VAR_ID",.before = "3002")
+quintiles<-ranked %>%
+  mutate(ONS_ID= rownames(ranked)) %>% 
+  relocate("ONS_ID",.before = "food_convenience_num_per_1000_res_plus_buffer")
 
 #colnames(compare3)
 
 #write CSV
-file_name <- paste0("outputs/pq outputs/processed/quintiles/FINAL_Quintiles_CensusProfile-", Sys.Date(),".csv")
+file_name <- paste0("outputs/pq outputs/processed/quintiles/FINAL_Quintiles_non_census-", Sys.Date(),".csv")
 readr::write_csv(quintiles, file_name)
 message("Results saved to ", file_name)
 
